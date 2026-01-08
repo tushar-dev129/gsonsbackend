ce77a2a9d139d4b1090c55b7cf02cb1c656b6340
anonymous9076 <vijaytushar915@gmail.com>
2026-01-09 03:37:08 +0530

    merge3

--- app.js
+++ app.js
@@ -7,17 +7,28 @@ const app = express();
 // route imports 
 
 const userRoute = require("./routes/userRoutes")
+const postRoute = require("./routes/postRoutes")
+const productRoute = require('./routes/productRoutes')
+const SavedRoute = require('./routes/savedRoutes')
+const categoryRoute = require('./routes/categoryRoutes')
+const contactRoute = require('./routes/contactRoutes')
 
 
 app.use(cors({
     origin: ["https://gsonsindia.com", "http://localhost:3000", "https://gsonsbackend.vercel.app"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
 app.use(express.json())
 app.use(cookieParser())
+app.options('*', cors());
 
 app.use("/gsons", userRoute);
+app.use("/gsons", postRoute);
+app.use("/gsons", productRoute);
+app.use("/gsons", SavedRoute);
+app.use("/gsons", categoryRoute);
+app.use("/gsons", contactRoute);
 
 
 // Middleware for error 

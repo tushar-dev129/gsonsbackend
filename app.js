af81ae74add8d4d2020ee7591e5dd04b03e902d2
Tushar kumar <68018920+anonymous9076@users.noreply.github.com>
2026-01-09 03:26:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const userRoute = require("./routes/userRoutes")
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://gsonsindia.com",
+    origin: process.env.FRONTEND_URL || "https://gsonsindia.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));

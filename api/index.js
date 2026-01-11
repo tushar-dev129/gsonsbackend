76d09cff96e33ef1a5d38ed3f89d7368de1d621f
anonymous9076 <vijaytushar915@gmail.com>
2026-01-12 02:21:45 +0530

    add api2

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 const app = require("../app");
+const dotenv = require('dotenv');
 const connectDatabase = require("../config/database");
+
+// config 
+dotenv.config({ path: 'config/config.env' })
 
 // Connect to database
 connectDatabase();
